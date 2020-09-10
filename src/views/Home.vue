49547c1993f93305006bf4ca2132a1a5641a652d
WkArtist <2550115859@qq.com>
2020-09-10 19:19:50 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// import FileSaver from 'file-saver'
+import ejs from 'ejs'
+export default {
+  name: 'Home',
+  mounted () {
+    // var aFileParts = ['<a id="a"><b id="b">hey!</b></a>']
+    // var oMyBlob = new Blob(aFileParts, { type: 'text/plain;charset=utf-8' })
+    // FileSaver.saveAs(oMyBlob, 'test.vue')
+    const people = ['geddy', 'neil', 'alex']
+    const html = ejs.render('<%= people.join(", "); %>', { people: people })
+    console.log(html)
+  }
+}
+</script>
